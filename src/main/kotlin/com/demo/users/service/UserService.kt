30306e7222a51d0b8038c9674c055fd7c4cc3202
Peter Mikkelsen <peter.mikkelsen@paymenttools.com>
2024-02-11 00:37:31 +0100

    :fire: Remove testing trace

--- src/main/kotlin/com/demo/users/service/UserService.kt
+++ src/main/kotlin/com/demo/users/service/UserService.kt
@@ -3,28 +3,27 @@ package com.demo.users.service
 import com.demo.users.User
 import com.demo.users.storage.UserRepository
 import com.demo.users.storage.UserTable
-import com.demo.utils.tracing.traced
 import org.jetbrains.exposed.sql.ResultRow
 import java.util.*
 
 class UserService(private val userRepository: UserRepository) {
 
-    fun createNewUser(user: User): Triple<UUID, String, String> = traced("createNewUser") {
+    fun createNewUser(user: User): Triple<UUID, String, String> {
         val row = userRepository.insert(user)
         val id = row[UserTable.id].value
         val created = row[UserTable.created].toString()
         val status = row[UserTable.status].toString()
-        Triple(id, created, status)
+        return Triple(id, created, status)
     }
 
-    fun getUserById(id: UUID): Triple<User, String, String> = traced("getUserById") {
+    fun getUserById(id: UUID): Triple<User, String, String> {
         val row = userRepository.findById(id)
         val lastModified = row[UserTable.updated].toString()
         val status = row[UserTable.status].toString()
-        Triple(row.toDomain(), lastModified, status)
+        return Triple(row.toDomain(), lastModified, status)
     }
 
-    fun markUserAsInactive(id: UUID) = traced("markUserAsInactive") {
+    fun markUserAsInactive(id: UUID) {
         userRepository.markAsInactive(id)
     }
 
